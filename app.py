e4ec3348f61c4f75f7a04570c3cb5834efb6a476
Sabeer MZ <sabeer@gcloud.com>
2019-06-29 18:19:34 +0000

    text added Happy k8s!

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Nice workto Kubernetes!'
+    return 'Happy Kubernetes!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
