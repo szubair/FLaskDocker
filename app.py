8b2d798dbb796aa23c2b4179a53703fdfe9bcf0c
Sabeer MZ <sabeertvc@gmail.com>
2019-07-01 12:48:21 +0530

    changed text

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Happy to see you all!!'
+    return 'Happy to see you all'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
