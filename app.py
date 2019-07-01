bbdfff878d612026231fabe5b421f0721d185cff
Sabeer MZ <sabeertvc@gmail.com>
2019-07-01 12:45:33 +0530

    changed text

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Happy Kubernetes!'
+    return 'Happy to see you all!!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
